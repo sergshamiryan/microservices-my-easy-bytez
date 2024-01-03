2f9bf5c080de0ad1b10a9feacaa8427521a8df57
ssham <sergshamiryan@gmail.com>
2024-01-03 19:46:50 +0400

    Config Server ADDED

---
+++ configserver/src/test/java/serg/shamiryan/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package serg.shamiryan.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
