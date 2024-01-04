2ffa2456c5e6dddc6ce959ff888b63c587301a6a
ssham <sergshamiryan@gmail.com>
2024-01-04 15:40:07 +0400

    Spring Cloud Config Server Added

--- accounts/src/main/java/serg/shamiryan/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/serg/shamiryan/accounts/dto/AccountsContactInfoDto.java
@@ -1,22 +1,18 @@
 package serg.shamiryan.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(
-        String profile,
-        String message,
-        Map<String, String> contactDetails,
-        List<String> onCallSupport,
-        Family family) {
-
-    record Family(Member member) {
-        record Member(FullName fullName) {
-            record FullName(String name, String surname) {
-            }
-        }
-    }
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+    private String profile;
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
